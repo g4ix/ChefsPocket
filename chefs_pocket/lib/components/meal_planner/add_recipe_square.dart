870c74cabdb6ec524b8bfa83c456241369639db1
uruthia <maria.elena.zuliani@gmail.com>
2024-05-16 14:38:33 +0200

    showdialog for meal planner

---
+++ chefs_pocket/lib/components/meal_planner/add_recipe_square.dart
@@ -0,0 +1,73 @@
+import 'package:chefs_pocket/components/recipe_card.dart';
+import 'package:chefs_pocket/components/saved/recipe_saved_element.dart';
+import 'package:chefs_pocket/config.dart';
+import 'package:chefs_pocket/models/recipe.dart';
+import 'package:dotted_border/dotted_border.dart';
+import 'package:flutter/material.dart';
+
+class AddRecipeSquare extends StatefulWidget {
+  Function addRecipeToMeal;
+  AddRecipeSquare({required this.addRecipeToMeal});
+  @override
+  _AddRecipeSquareState createState() => _AddRecipeSquareState();
+}
+
+class _AddRecipeSquareState extends State<AddRecipeSquare> {
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('Seleziona una Ricetta'),
+              content: Container(
+                height: double.maxFinite ,
+                width: double.maxFinite,
+                child: ListView.builder(
+                    itemCount: allSavedRecipes.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return InkWell(
+                        onTap: () {
+                          widget.addRecipeToMeal(allSavedRecipes[index]);
+                          Navigator.pop(context);
+                        },
+                        child: Container(
+                          height: MediaQuery.of(context).size.height * 0.22,
+                    width: MediaQuery.of(context).size.width * 0.8,
+                          child: RecipeCard(
+                            recipe: allSavedRecipes[index],
+                            modModify: false,
+                          ),
+                        ),
+                      );
+                    }),
+              ),
+            );
+          },
+        );
+      },
+      child: Container(
+        child: DottedBorder(
+          padding: EdgeInsets.all(0),
+          radius: Radius.circular(10),
+          color: Color(0xFF557F9F),
+          dashPattern: [5, 5],
+          borderType: BorderType.RRect,
+          strokeWidth: 2,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Color(0xFF557F9F).withOpacity(0.2),
+              borderRadius: BorderRadius.circular(5),
+              border: Border.all(color: Color(0xFF557F9F).withOpacity(0.2)),
+            ),
+            child: Center(
+              child: Icon(Icons.add, color: Color(0xFF557F9F), size: 30),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
